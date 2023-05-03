6bee188bf8d1b3ee5140dd9f7e6e3e41d4cc0db4
Antonio Molinari <molinari@incode.it>
2023-05-03 13:00:51 +0200

    lesson19

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import CustomComponent from './components/CustomComponent.vue'
 </script>
 
 <template>
+  <CustomComponent />
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
---
+++ src/components/CustomComponent.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+ <div class="custom-component">
+  <div class="title">
+    i'm a custom component!
+  </div>
+ </div>
+</template>
+
+<style scoped>
+  .title {
+    font-size: 2em;
+  }
+</style>
