5dfe6fa159be4e903281b4c5b5dc0962d2bb7a24
ileniacauchioli <127940239+ileniacauchioli@users.noreply.github.com>
2023-05-31 10:15:44 +0200

    Create Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <img class="logo" src="./assets/logo.svg" alt="Logo" />
+    <div class="wrapper">
+      <h1>My Portfolio</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        voluptatum, voluptate, quibusdam, quia voluptas quod quos
+        exercitationem voluptatem quas quibusdam, quia voluptas quod quos
+        exercitationem voluptatem quas.
+      </p>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
